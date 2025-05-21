034978bea18e8d39e38412c6f7cd5c2a836d78a1
DataVizandAI <alanj2305@gmail.com>
2025-05-21 17:22:43 +0200

    update

---
+++ population/README.md
@@ -0,0 +1,21 @@
+# Plotly: a Highlighted Line Graph
+
+## How to identify the data you are interested in, but keep the context - a simple library function.
+
+[Medium]() [Substack]()
+
+Files to support the article.
+
+- ``st.py`` A Streamlit program that uses the highlight function.
+- ``stwithselector.pt`` A version of ``st.py`` with a selector for the countries.
+- ``rate.csv`` A semicolon separated file of the data
+- 
+
+---
+
+##### If you find this content useful, please consider one or more of the following:
+
+- ###### Subscribe to my [Substack](https://technofile.substack.com/) and/or Follow me on  [Medium](https://medium.com/@alan-jones) where you can read articles along with thousands of others for $5 a month
+- ###### Buy a book from my bookstore on Bookstore.org ([UK](https://uk.bookshop.org/lists/data-visualization-and-data-science?), [US](https://bookshop.org/lists/data-visualization-and-data-science)) - _supporting local bookstores, not billionaires_.
+
+<a href='https://ko-fi.com/M4M64THKG' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
---
+++ population/st.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+def highlight_chart(df, 
+                    x_value, 
+                    y_value, 
+                    trace_name, 
+                    highlight_traces, 
+                    xaxis_title="", yaxis_title="", chart_name="", 
+                    width=1600, height=800, template='plotly_white'):
+    """
+    Generates a Plotly figure with highlighted traces.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame containing the data.
+        x_value (str): The name of the column to use for the x-axis.
+        y_value (str): The name of the column to use for the y-axis.
+        trace_name (str): The name of the column that identifies different traces (e.g., countries).
+        highlight_traces (list): A list of trace names to highlight with different colors.
+        xaxis_title (str, optional): Title for the x-axis. Defaults to "".
+        yaxis_title (str, optional): Title for the y-axis. Defaults to "".
+        chart_name (str, optional): Title for the chart. Defaults to "".
+        width (int, optional): Width of the chart in pixels. Defaults to 1600.
+        height (int, optional): Height of the chart in pixels. Defaults to 800.
+        template (str, optional): Plotly template to use. Defaults to 'plotly_white'.
+
+    Returns:
+        plotly.graph_objects.Figure: The generated Plotly figure.
+    """
+    # Create the base figure
+    fig = go.Figure()
+
+    # Plot all traces in grey
+    for t_name in df[trace_name].unique():
+        t_df = df[df[trace_name] == t_name]
+        fig.add_trace(go.Scatter(
+            x=t_df[x_value],
+            y=t_df[y_value],
+            mode='lines',
+            line=dict(color='#E8E8E8', width=1),
+            showlegend=False  # Hide legend for grey lines
+        ))
+
+
+    # Overlay selected countries in color
+    colors = px.colors.qualitative.Plotly
+    for t_name, color in zip(highlight_traces, colors):
+        t_df = df[df[trace_name] == t_name]
+        fig.add_trace(go.Scatter(
+            x=t_df[x_value],
+            y=t_df[y_value],
+            mode='lines',
+            name=t_name,
+            line=dict(color=color, width=4)
+        ))
+
+    # Update layout
+    fig.update_layout(
+        title=chart_name,
+        xaxis_title=xaxis_title,
+        yaxis_title=yaxis_title,
+        height = height,
+        width = width,
+        template=template
+    )
+    return fig
+
+# Load the data
+df = pd.read_csv('rate.csv', sep=';')
+
+chart_name='Fertility Rate by Country'
+xaxis_title='Year'
+yaxis_title='Fertiity Rate'
+template='simple_white'
+
+# column names
+y_value = 'Fertility Rate'
+x_value = 'Year'
+trace_name = 'Region'
+
+
+# List of countries to highlight
+highlight_traces = ['World','Spain','India','Brazil','United States of America']
+
+fig = highlight_chart(df,x_value=x_value, y_value=y_value, 
+                      trace_name=trace_name, 
+                      highlight_traces=highlight_traces,
+                      xaxis_title=xaxis_title,
+                      yaxis_title=yaxis_title,
+                      chart_name=chart_name) 
+
+st.plotly_chart(fig, use_container_width=True)
+
+
